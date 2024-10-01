fde301733ae01dc649e86c9c2e5aba848a8f60e5
sansalaR <136149447+sansalaR@users.noreply.github.com>
2024-10-01 15:58:35 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import CounterApp from './counterApp'; 
+
+function App() {
+    return (
+        <div>
+            <CounterApp />
+        </div>
+    );
+}
+
+export default App;
